8edf98915b1482bc27d89ae2ec84fbaafcfbab4b
Abebaw-Tsega <abebawtsega3@gmail.com>
2024-12-09 22:05:58 +0300

    amazon deploy on netflix

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-3770a/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-3770a/us-central1/api",
+  baseURL: "https://amazone-deploy-1-2qv9.onrender.com",
+
+
 });
 
 export { axiosInstance };
